cbe26aa28e2e2c732f95b335bf36a3ef5fa090ad
Yaokai Liu <yaokai.liu@foxmail.com>
2025-05-21 19:34:21 +0800

    update project description.

--- grammar/regex/context.h
+++ grammar/regex/context.h
@@ -1,6 +1,6 @@
 /* License
  *
- * ${PROJ_DESCRIPTION}
+ * xRegex - a Kind of Regular Expression
  * Copyright (C) 2025 Yaokai Liu
  *
  * This program is free software: you can redistribute it and/or modify
